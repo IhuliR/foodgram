ee4666d4791c3291a98e297cdb5d8113afb62dc2
ilia Roshchin <neo-ily81@yandex.ru>
2026-04-06 16:07:33 +0300

    linters and frontend_tests

--- backend/foodgram_backend/api/filters.py
+++ backend/foodgram_backend/api/filters.py
@@ -1,12 +1,11 @@
 from django_filters import rest_framework as filters
-
 from recipes.models import Ingredient, Recipe
 
 
 class IngredientFilter(filters.FilterSet):
     name = filters.CharFilter(
         field_name='name',
-        lookup_expr='istartswith'        
+        lookup_expr='istartswith'
     )
 
     class Meta:
